1da01b431b8edbbc830cd3726df2fbb885741a14
devsunnysingh <sunnysingh.hj07@gmail.com>
2024-07-11 00:41:52 -0400

    Factorial using recursion

--- src/com/algorithmpractice/recursion/practice/easy/Factorial.java
+++ src/com/algorithmpractice/recursion/practice/easy/Factorial.java
@@ -1,7 +1,19 @@
 package src.com.algorithmpractice.recursion.practice.easy;
 
 public class Factorial {
+    public static int factorial(int n){
+        int fact=1;
+        if(n==0 || n==1){
+            return fact;
+        }
+        fact=n*factorial(n-1);
+        return fact;
+
+
+    }
     public static void main(String[] args) {
+        System.out.println(factorial(5));
+
 
     }
 }
