e2e4686488f94bf18b80cc969e2ba0a1356cc31d
98sunny <sunnysingh.hj07@gmail.com>
2024-07-11 21:18:00 -0400

    Reverse number using palindrome

--- src/com/algorithmpractice/recursion/practice/easy/ReverseNumbers.java
+++ src/com/algorithmpractice/recursion/practice/easy/ReverseNumbers.java
@@ -10,9 +10,22 @@ public class ReverseNumbers {
         sum= (sum * 10) + rem;
         reverseUsingRecursion(n/ 10);
     }
+//sometimes we need additional parameters in the arguments of the function.
+//  int that case, make another fiunction and pass the parameter as the argument of the function.
+    public static int reverseUsingRecursion(int n, int base){
+        if (n == 0) {
+            return 0;
+        }
+    int rem = 0;
+        rem=n % 10;
+        rem=(int)(rem * Math.pow(10, base)) + reverseUsingRecursion(n / 10, base - 1);
+        return rem;
+
+    }
     public static void main(String[] args) {
 //        System.out.println(reverseUsingRecursion(12345));
         reverseUsingRecursion(12345);
         System.out.println(sum);
+        System.out.println(reverseUsingRecursion(12345, 4));
     }
 }
