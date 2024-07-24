c3877bc7687342e7d7f98344c06173f83178f9a1
98sunny <sunnysingh.hj07@gmail.com>
2024-07-24 19:06:44 -0400

    Operations in singly LinkedList

--- src/com/linkedList/LinkedList.java
+++ src/com/linkedList/LinkedList.java
@@ -13,6 +13,7 @@ public class LinkedList {
         }
     }
 
+
     private Node head;
     private Node tail;
     private int size;
@@ -27,6 +28,74 @@ public class LinkedList {
         }
         size+=1;
     }
+    public void insertLast(int val){
+        if(tail==null){
+            insertFirst(val);
+        }
+        Node node=new Node(val);
+        tail.next=node;
+        node.next=null;
+        tail=node;
+        size=size+1;
+
+    }
+    public void insertAfterGivenPosition(int val, int position){
+
+        if(position==0){insertFirst(val);}
+        if(position==size){insertLast(val);}
+        Node temp=head;
+        for (int i = 1; i < position; i++) {
+            temp=temp.next;
+        }
+        Node node=new Node(val,temp.next);
+        temp.next=node;
+        size++;
+    }
+
+    public int deleteFirst(){
+        int val=head.value;
+        if(head==null){
+            head=tail=null;
+        }
+        head=head.next;
+        return val;
+    }
+    public Node get(int index){
+        Node node=head;
+        for(int i=1;i<index;i++){
+            node=node.next;
+        }
+        return node;
+
+    }
+    public int deleteLastNode(){
+        if(size>=1){
+            return deleteFirst();
+        }
+        Node secondLast=get(size-2);
+        int val=secondLast.value;
+        tail=secondLast;
+        tail.next=null;
+        return val;
+    }
+
+    public int deleteAGivenIndex(int position){
+         Node prev=get(position-1);
+         int val=prev.next.value;
+         prev.next=prev.next.next;
+         return val;
+    }
+    public Node find(int value){
+        Node node=head;
+        while (node.value!=value){
+            if(node.value==value){
+                return node;
+            }
+            node=node.next;
+        }
+        return null;
+
+    }
     public void displayList(){
         Node temp=head;
         while (temp!=null){
@@ -37,11 +106,13 @@ public class LinkedList {
         System.out.print("END");
 
     }
+
     public static void main(String[] args) {
         LinkedList list =new LinkedList();
         list.insertFirst(3);
         list.insertFirst(2);list.insertFirst(8);
-        list.insertFirst(17);
+        list.insertLast(17);
+        list.insertAfterGivenPosition(200,2);
         list.displayList();
 
 
